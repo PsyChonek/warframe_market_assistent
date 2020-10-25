26e9660dce84d816db4d5e060c21a647b5bf0974
PsyChonek <45714777+PsyChonek@users.noreply.github.com>
2020-10-26 00:05:31 +0100

    v0.4

--- market.py
+++ market.py
@@ -4,23 +4,65 @@ import time
 from os import system, name
 system('cls')
 
-
-root_url = "https://api.warframe.market/v1/"
-headers = {'language' : 'en','accept' : 'application/json', 'platform' : 'pc'}
-
 def request(target):
+    root_url = "https://api.warframe.market/v1/"
+    headers = {'language' : 'en','accept' : 'application/json', 'platform' : 'pc'}
+    time.sleep(0.35)
     temp = requests.get(root_url+target,headers=headers)
     if temp:
-        print()
+        return temp
     else:
         print(root_url + target + ": Error 404!")
         exit()
-    return temp
+
+def liststatistics(list):
+    items_list = []
+    items_list_json = request("items").json()['payload']['items']
+    for name in items_list_json:
+        for list_name in list:
+            if name["item_name"] == list_name:
+                price_avg = 0
+                sold_volume = 0
+                price_min = 10000
+                price_max = 0
+
+                items_statistics_json = request("items/" + name["url_name"] + "/statistics").json()
+                for record in items_statistics_json['payload']['statistics_closed']['48hours']:
+                    if ("mod_rank" not in record or record["mod_rank"] == 0) and record["volume"] > 0:
+                        sold_volume += record["volume"]
+                        if price_min > record["median"]:
+                            price_min = record["median"]
+                        if price_max < record["median"]:
+                            price_max = record["median"]
+                        if price_avg > 0:
+                            price_avg = (price_avg+(record["volume"]*record["median"]))/((record["volume"]+1))
+                        else:
+                            price_avg = (price_avg+(record["volume"]*record["median"]))/((record["volume"]))
+
+                items_list.append(item(list_name, name["url_name"], str(sold_volume), str(price_min), str(price_max), str(round(price_avg, 0))))
+
+    return items_list
+
+def itemorders(item_url,player_name):
+    item_orders_json = request("items/" + item_url + "/orders").json()['payload']['orders']
+    min_price = 10000
+    for order in item_orders_json:
+        if order["user"]["status"] == "ingame" and order["order_type"] == "sell" and order["user"]["ingame_name"] != player_name:
+            if min_price > order["platinum"]:
+                min_price = order["platinum"]
+    return str(min_price)
+
+class item:
+  def __init__(self, name, url, sold, min, max, avg):
+    self.name = name
+    self.url = url
+    self.sold = sold
+    self.min = min
+    self.max = max
+    self.avg = avg
 
 def itemscheck(list_src):
-
-    items_url_json = request("items").json()
-
+    items_list = []
     list = []
     file = open(list_src, "r")
     list = file.read().splitlines()
@@ -31,70 +73,44 @@ def itemscheck(list_src):
         list[i] = list[i].title()
 
     print(list)
+    items_list = liststatistics(list)
 
-    for name in items_url_json['payload']['items']:
-        for list_name in list:
-            if name["item_name"] == list_name:
-                avg = 0
-                sold_volume = 0
-                price_min = 1000
-                price_max = 0
+    for item in items_list:
+        print()
+        print(item.name)
+        print("Sold: "+item.sold)
+        print("Min: "+item.min)
+        print("Max: "+item.max)
+        print("Avg: "+item.avg)
 
-                time.sleep(0.35)
-                item_url_json = request("items/" +name["url_name"]+"/statistics").json()
-                for record in item_url_json['payload']['statistics_closed']['48hours']:
-                    if "mod_rank" not in record or record["mod_rank"] == 0:
-                        avg += record["median"]
-                        sold_volume += record["volume"]
-                        if price_min > record["median"]:
-                            price_min = record["median"]
-                            if price_max < record["median"]:
-                                price_max = record["median"]
 
-                print(list_name)
-                print("Sold: " + str(sold_volume))
-                if sold_volume > 0:
-                    print("Avg: " + str(avg/sold_volume))
-                    print("Min: " + str(price_min))
-                    print("Max: " + str(price_max))
 
 def playerorderschecks(player_name):
-
-    items_url_json = request("items").json()
-    trade_list = []
-    time.sleep(0.35)
-    player_json = request("profile/" + player_name+ "/orders").json()
-    for order in player_json['payload']['sell_orders']:
-        trade_list.append(order['item']['en']["item_name"])
     P = -1
-    for name in items_url_json['payload']['items']:
-        for list_name in trade_list:
+    list = []
+    items_list = []
+    player_json = request("profile/" + player_name + "/orders").json()
 
-            if name["item_name"] == list_name:
-                avg = 0
-                sold_volume = 0
-                price_min = 1000
-                price_max = 0
-                P +=1
+    for order in player_json['payload']['sell_orders']:
+        list.append(order['item']['en']["item_name"])
 
-                time.sleep(0.35)
-                item_url_json = request("items/" +name["url_name"]+"/statistics").json()
-                for record in item_url_json['payload']['statistics_closed']['48hours']:
-                    if "mod_rank" not in record or record["mod_rank"] == 0:
-                        avg += record["median"]
-                        sold_volume += record["volume"]
-                        if price_min > record["median"]:
-                            price_min = record["median"]
-                            if price_max < record["median"]:
-                                price_max = record["median"]
+    items_list = liststatistics(list)
 
-                print(list_name)
-                print("My price: " + str(player_json['payload']['sell_orders'][P]['platinum']))
-                print("Sold: " + str(sold_volume))
-                if sold_volume > 0:
-                    print("Avg: " + str(avg/sold_volume))
-                    print("Min: " + str(price_min))
-                    print("Max: " + str(price_max))
+    for item in items_list:
+        print()
+        print(item.name)
+        print("My price: " + str(player_json['payload']['sell_orders'][P]['platinum']))
+        print("Others min: " + itemorders(item.url,player_name))
+        print("Sold: "+item.sold)
+        print("Min: "+item.min)
+        print("Max: "+item.max)
+        print("Avg: "+item.avg)
+        P += 1
+
+
+
+
+
 
 def mode(input1):
     if input1 == "1":
@@ -107,7 +123,7 @@ def mode(input1):
         playerorderschecks(input1)
 
 #    print(json.dumps(player_json, indent=4, sort_keys=True))
-# user input
+#       user input
 
 print("Hello, select what u want.")
 print("1 - Parts | 2 - Cephalon | 3 - Mods | Player name")
