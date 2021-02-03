1d71c15aef18d9b6ad2cfb9b0ed751dc7fa87f26
PsyChonek <45714777+PsyChonek@users.noreply.github.com>
2021-02-03 16:12:17 +0100

    Update market.py

--- market.py
+++ market.py
@@ -4,11 +4,34 @@ import time
 from os import system, name
 system('cls')
 
+JWT = "JWT=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzaWQiOiJwV3BsZ0lWQkhpTzFjMDhxQko5V1huVUNpVG9aTDVoeSIsInNlY3VyZSI6dHJ1ZSwiand0X2lkZW50aXR5IjoiU2xqTnBUY3h1UjlSeHRWOHI4WVVxWGQ3Q1pxeFE2Q1AiLCJsb2dpbl91YSI6ImIncHl0aG9uLXJlcXVlc3RzLzIuMjQuMCciLCJsb2dpbl9pcCI6ImInOTAuMTgwLjE1MC43NyciLCJhdXRoX3R5cGUiOiJoZWFkZXIiLCJleHAiOjE2MDkwODc5NTMsImlhdCI6MTYwMzkwMzk1MywiaXNzIjoiand0IiwiYXVkIjoiand0In0.oO_TXJw6BMmxWO3qmvT1gf7F9QeahQeO3F1p-tR1mdE"
+
+def signin():
+    email = input()
+    pw = input()
+    jsonlogin={'email':email, 'password':pw, 'auth_type':'header'}
+    headers={'Authorization' : JWT, 'language' : 'en', 'accept' : 'application/json', 'platform' : 'pc', 'auth_type' : 'header'}
+    request= requests.post('https://api.warframe.market/v1/auth/signin',headers=headers,json=jsonlogin)
+    json_profile = request.json()
+    print (request.headers['Authorization'])
+    print(json.dumps(json_profile, indent=4, sort_keys=True))
+
 def request(target):
     root_url = "https://api.warframe.market/v1/"
-    headers = {'language' : 'en','accept' : 'application/json', 'platform' : 'pc'}
+    headers={'Authorization' : JWT, 'language' : 'en', 'accept' : 'application/json', 'platform' : 'pc', 'auth_type' : 'header'}
     time.sleep(0.35)
     temp = requests.get(root_url+target,headers=headers)
+    if temp:
+        return temp
+    else:
+        print(root_url + target + ": Error 404!")
+        exit()
+
+def order_change(target):
+    root_url = "https://api.warframe.market/v1/"
+    headers={'Authorization' : JWT, 'language' : 'en', 'accept' : 'application/json', 'platform' : 'pc', 'auth_type' : 'header'}
+
+    temp = requests.post(root_url+target,headers=headers,data=payload)
     if temp:
         return temp
     else:
@@ -97,6 +120,35 @@ def playerorderschecks(player_name):
     items_list = liststatistics(list)
 
     for item in items_list:
+        print
+        print(item.name)
+        print("My price: " + str(player_json['payload']['sell_orders'][P]['platinum']))
+        print("Others min: " + itemorders(item.url,player_name))
+        print("Sold: "+item.sold)
+        print("Min: "+item.min)
+        print("Max: "+item.max)
+        print("Avg: "+item.avg)
+        P += 1
+
+def pricefixer(player_name):
+    P = -1
+    list = []
+    orders_id = []
+    items_list = []
+    player_json = request("profile/" + player_name + "/orders").json()
+
+    for order in player_json['payload']['sell_orders']:
+        list.append(order['item']['en']["item_name"])
+        orders_id.append(order["id"])
+        print (order["platinum"])
+        print(json.dumps(order, indent=4, sort_keys=True))
+
+
+
+
+
+    items_list = liststatistics(list)
+    for item in items_list:
         print()
         print(item.name)
         print("My price: " + str(player_json['payload']['sell_orders'][P]['platinum']))
@@ -108,7 +160,9 @@ def playerorderschecks(player_name):
         P += 1
 
 
-
+def getplayername():
+    print("Username:")
+    return input()
 
 
 
@@ -119,14 +173,20 @@ def mode(input1):
         itemscheck("CephalonSimaris75.txt")
     elif input1 == "3":
         itemscheck("Mods.txt")
+    elif input1 == "4":
+        playerorderschecks("Mr.PsyChonek")
+    elif input1 == "5":
+        pricefixer('Mr.PsyChonek')
+    elif input1 == "6":
+        signin()
     else:
-        playerorderschecks(input1)
+        exit()
 
 #    print(json.dumps(player_json, indent=4, sort_keys=True))
 #       user input
 
 print("Hello, select what u want.")
-print("1 - Parts | 2 - Cephalon | 3 - Mods | Player name")
+print("1 - Parts | 2 - Cephalon | 3 - Mods | 4 - Checks orders | 5 - Order price auto | 6 - Signin")
 input1 = ""
 if input1 == "":
     input1 = input()
